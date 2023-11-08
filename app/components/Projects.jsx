bd37f055cc6742750b7f90e64bf93703b796cee1
Ash Uppal <ashuppal@gmail.com>
2023-11-08 13:49:11 -0800

    added github and preview for projects

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -9,6 +9,8 @@ const projectsData = [
     id: 1,
     image: "/images/superQuiz.png",
     tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "https://getsuperquiz.com/"
   },
   {
     id: 2,
@@ -16,7 +18,8 @@ const projectsData = [
     description: "Transcribe audio files and create PowerPoint presentations with ease.",
     image: "/images/audioSpark.png",
     tag: ["All", "Web"],
-    gitUrl:"/"
+    gitUrl: "https://github.com/ashuppal/whisperPPT",
+    previewUrl: "https://audiospark.onrender.com/"
   },
   {
     id: 3,
@@ -24,6 +27,8 @@ const projectsData = [
     description: "Creates a tailored resume and cover letter that specifically highlights your relevant experience and qualifications for that job.",
     image: "/images/polishedProfile.png",
     tag: ["All", "Web"],
+    gitUrl: "https://github.com/PolishedProfiles",
+    previewUrl: "https://polished-profiles.netlify.app/"
   },
   {
     id: 4,
@@ -31,19 +36,29 @@ const projectsData = [
     description: "Play an Adventure Game",
     image: "/images/superQuiz.png",
     tag: ["All", "Mobile"],
+    gitUrl: "/",
+    previewUrl: "/"
   },
 
 ];
 function Projects() {
   return (
-<>
-<h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+    <>
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
         My Projects
       </h2>
-<div> {projectsData.map((project) => <Cards key={project.id} title={project.title} description={project.description} imgUrl = {project.image} />)}
-</div>
-</>
-   
+      <div> {projectsData.map((project) =>
+       <Cards
+        key={project.id} 
+        title={project.title}
+         description={project.description} 
+         imgUrl={project.image}
+         gitUrl={project.gitUrl}
+         previewUrl={project.previewUrl}
+          />)}
+      </div>
+    </>
+
   )
 }
 
