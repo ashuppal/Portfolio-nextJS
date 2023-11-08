bd7b4163e9bb2e18d5fcd0c461832ecf27a7e362
Ash Uppal <ashuppal@gmail.com>
2023-11-08 11:39:21 -0800

    fixed the issue of about me tabs

--- app/components/AboutMe.jsx
+++ app/components/AboutMe.jsx
@@ -71,14 +71,24 @@ function AboutMe() {
     });
   };
   return (
-    <section className="text-white">
-      <div className="gap-8  py-8 px-4 xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-16 mt-4">
-      
-        <Image src="/images/hero.png" width={700} height={700} alt="aboutme" />
-    
-        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
-          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
-          <p className="text-white texxt-base md:text-lg">
+<section className="text-white">
+      <div className="container mx-auto py-8 px-4">
+        <div className="flex flex-col md:flex-row items-center justify-center md:justify-start gap-2 md:gap-4 xl:gap-6 mb-6">
+          <div className="w-full md:w-1/2 flex justify-center">
+            <div className="w-64 md:w-96">
+              <Image 
+                src="/images/hero.png" 
+                alt="aboutme" 
+                width={600} 
+                height={600} 
+                layout="responsive" 
+                objectFit="contain"
+              />
+            </div>
+          </div>
+          <div className="w-full md:w-1/2 text-center md:text-left">
+            <h2 className="text-4xl font-bold mb-4">About Me</h2>
+            <p className="text-base md:text-lg mb-6">
            My journey in the
             tech world is fueled by a desire to develop solutions that make a
             difference, backed by a solid foundation in both front-end and
@@ -90,8 +100,11 @@ function AboutMe() {
             product development, enabling me to intuitively align software
             functionalities with customer needs, and to deliver personalized
             solutions that resonate with users. 
-          </p>
-          <div className="flex flex-row justify-center mt-8">
+            </p>
+          </div>
+        </div>
+        <div className="w-full flex flex-col items-center">
+          <div className="flex justify-center mt-4 mb-8 gap-6">
             <AboutMeTabs
               selectTab={() => handleTabChange("skills")}
               active={tab === "skills"}
@@ -111,8 +124,10 @@ function AboutMe() {
               Education
             </AboutMeTabs>
           </div>
-          <div className="mt-8">
-            {TAB_DATA.find((t) => t.id === tab).content}
+          <div className="mt-8 w-full flex justify-center">
+            <div className="w-full max-w-xl px-4"> {/* Adjust max-width as necessary */}
+              {TAB_DATA.find((t) => t.id === tab).content}
+            </div>
           </div>
         </div>
       </div>
