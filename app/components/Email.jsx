0259dae9445be978cce828f959abacea804eb5bd
Ash Uppal <ashuppal@gmail.com>
2023-11-11 13:00:46 -0800

    added socials on the hero section

--- app/components/Email.jsx
+++ app/components/Email.jsx
@@ -14,7 +14,7 @@ const Email = () => {
     <section  id="contact" className="grid md:grid-cols-2 my-12 md:my-16 py-24 gap-4 relative">
       <div className="absolute w-80 h-80 z-0 "></div>
       <div className="z-10">
-        <h5 className="text-xl font-bold text-white my-2">Let's Connect</h5>
+        <h5 className="text-xl font-bold text-white myåç-2">Let's Connect</h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
           I'm currently looking for new opportunities, my inbox is always open.
           Whether you have a question or just want to say hi, I'll try my best
--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -2,46 +2,76 @@
 import Link from 'next/link';
 import React from 'react';
 import { TypeAnimation } from 'react-type-animation';
-
+import Image from "next/image";
+import GithubIcon from "../../public/images/github2.png"
+import LinkedinIcon from "../../public/images/LinkedIn.png"
 const HeroSection = () => {
+  const handleDownloadCV = () => {
+    window.open('/resume.pdf', '_blank');
+  };
+  const openInNewTab = (url) => {
+    window.open(url, '_blank', 'noopener,noreferrer');
+  };
   return (
     <section className="relative grid grid-cols-1 lg:grid-cols-12 my-4">
       <div className="col-span-7">
         <div className="pb-20">
-          <h1 className="mb-4 text-3xl lg:text-6xl font-extrabold text-white">
-            <span className="bg-clip-text text-transparent bg-gradient-to-r from-cyan-500 to-lime-500">
-              Hello, I am Ashwini
-            </span>
-        
+          <div className="flex items-center mb-4">
+            <h1 className="text-3xl lg:text-6xl font-extrabold text-white inline">
+              <span className="bg-clip-text text-transparent bg-gradient-to-r from-cyan-500 to-lime-500">
+                Hello, I am Ashwini
+              </span>
+            </h1>
+            <div className="flex gap-2 ml-4">
+              <div className="cursor-pointer" onClick={() => openInNewTab('https://github.com/ashuppal')}>
+                <Image
+                  alt="GitHub logo"
+                  src={GithubIcon}
+                  width={30}
+                  height={40}
+                  layout="fixed"
+                />
+              </div>
+              <div className="cursor-pointer" onClick={() => openInNewTab('https://www.linkedin.com/in/ashwini-uppal')}>
+                <Image
+                  alt="LinkedIn logo"
+                  src={LinkedinIcon}
+                  width={28}
+                  height={30}
+                  layout="fixed"
+                />
+              </div>
+            </div>
+          </div>
             <TypeAnimation
               sequence={[
-                'Full Stack Web Developer', 2000, 
-                'Creator of SuperQuiz', 1000, 
-                'Experience in JavaScript, TypeScript, C#, Python', 1000, 
-                'React, React Native, Express.js', 1000, 
-                'MongoDB, PostgreSQL, AWS, MUI, Tailwind CSS', 1000, 
+                'Full Stack Web Developer', 2000,
+                'Creator of SuperQuiz', 1000,
+                'Experience in JavaScript, TypeScript, C#, Python', 1000,
+                'React, React Native, Express.js', 1000,
+                'MongoDB, PostgreSQL, AWS, MUI, Tailwind CSS', 1000,
                 'HTTP API Clients, OpenAI GPT APIs, Auth0 Authentication', 1000
               ]}
               wrapper="div"
-              style={{ fontSize: '1.625rem', lineHeight: '2.25rem' }}
+              style={{ fontSize: '1.625rem', lineHeight: '2.25rem', color:'white' }}
               speed={50}
               backSpeed={60}
               repeat={Infinity}
             />
-          </h1>
         </div>
-  
+
         <div className="flex flex-col lg:flex-row space-y-4 lg:space-y-0 lg:space-x-4 p-4">
-          <Link href="#contact" className="inline-flex justify-center bg-gradient-to-br from-cyan-500 to-lime-500 text-charcoal rounded-full shadow-lg transition duration-150 ease-in-out">
-            <span className="block bg-charcoal hover:bg-gray-900 rounded-full px-5 py-2 whitespace-nowrap">
+          <Link href="#contact" className="inline-flex justify-center items-center bg-gradient-to-br from-cyan-500 to-lime-500 text-charcoal rounded-full shadow-lg transition duration-150 ease-in-out transform hover:scale-105">
+            <span className="block bg-charcoal hover:bg-cyan-500 hover:border-cyan-700 hover:border-2 hover:text-white rounded-full px-5 py-2 whitespace-nowrap">
               Hire Me
             </span>
           </Link>
-          <Link href="/" className="inline-flex justify-center bg-gradient-to-br from-cyan-500 to-lime-500 text-charcoal rounded-full shadow-lg transition duration-150 ease-in-out">
-            <span className="block bg-charcoal hover:bg-gray-900 rounded-full px-5 py-2 whitespace-nowrap">
-              Download CV
+
+          <span onClick={handleDownloadCV} className="inline-flex justify-center bg-gradient-to-br from-cyan-500 to-lime-500 text-charcoal rounded-full shadow-lg transition duration-150 ease-in-out cursor-pointer">
+            <span className="block bg-charcoal hover:bg-cyan-500 hover:border-cyan-700 hover:border-2 hover:text-white rounded-full px-5 py-2 whitespace-nowrap">
+              Download Resume
             </span>
-          </Link>
+          </span>
         </div>
       </div>
     </section>
