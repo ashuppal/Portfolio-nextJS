0d8088e13a604a5f5e14d6a2540bb53fd9e3700d
Ash Uppal <ashuppal@gmail.com>
2023-11-13 11:20:54 -0800

    form component working as expected

--- app/components/AboutMe.jsx
+++ app/components/AboutMe.jsx
@@ -71,40 +71,40 @@ function AboutMe() {
     });
   };
   return (
-<section className="text-white" id="about">
+    <section className="text-white" id="about">
       <div className="container mx-auto py-8 px-4">
         <div className="flex flex-col md:flex-row items-center justify-center md:justify-start gap-2 md:gap-4 xl:gap-6 mb-6">
           <div className="w-full md:w-1/2 flex justify-center">
             <div className="w-64 md:w-96">
-              <Image 
-                src="/images/hero.png" 
-                alt="aboutme" 
-                width={600} 
-                height={600} 
-                layout="responsive" 
+              <Image
+                src="/images/hero.png"
+                alt="aboutme"
+                width={600}
+                height={600}
+                layout="responsive"
                 objectFit="contain"
               />
             </div>
           </div>
-          <div className="w-full md:w-1/2 text-center md:text-left">
-            <h2 className="text-4xl font-bold mb-4">About Me</h2>
+          <div className="w-full md:w-1/2 text-center md:text-left mt-2 md:mt-4">
+            <h2 className="text-4xl font-bold mb-1">About Me</h2>
             <p className="text-base md:text-lg mb-6">
-           My journey in the
-            tech world is fueled by a desire to develop solutions that make a
-            difference, backed by a solid foundation in both front-end and
-            back-end development, and a commitment to delivering exceptional
-            digital experiences. <br></br>By launching SuperQuiz,
-            I've refined my leadership and innovation capabilities, specializing
-            in the creation of engaging, AI-assisted quizzes tailored to enhance
-            educational experiences. This experience has sharpened my skills in
-            product development, enabling me to intuitively align software
-            functionalities with customer needs, and to deliver personalized
-            solutions that resonate with users. 
+              My journey in the
+              tech world is fueled by a desire to develop solutions that make a
+              difference, backed by a solid foundation in both front-end and
+              back-end development, and a commitment to delivering exceptional
+              digital experiences. <br></br>By launching SuperQuiz,
+              I've refined my leadership and innovation capabilities, specializing
+              in the creation of engaging, AI-assisted quizzes tailored to enhance
+              educational experiences. This experience has sharpened my skills in
+              product development, enabling me to intuitively align software
+              functionalities with customer needs, and to deliver personalized
+              solutions that resonate with users.
             </p>
           </div>
         </div>
-        <div className="w-full flex flex-col items-center">
-          <div className="flex justify-center mt-4 mb-8 gap-6">
+        <div className="w-full flex flex-col items-center mt-8 md:mt-12">
+          <div className="flex justify-center mt-4 mb-3 gap-6">
             <AboutMeTabs
               selectTab={() => handleTabChange("skills")}
               active={tab === "skills"}
--- app/components/Email.jsx
+++ app/components/Email.jsx
@@ -1,25 +1,59 @@
-'use client'; 
-import React from 'react';
+'use client';
+import React, {useState} from 'react';
 import Image from "next/image";
 import Link from "next/link";
 import GithubIcon from "../../public/images/github2.png"
 import LinkedinIcon from "../../public/images/LinkedIn.png"
 
 const Email = () => {
+
+  const [emailSubmitted, setEmailSubmitted] = useState(false);
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+   const data = {
+      email: e.target.email.value,
+      subject: e.target.subject.value,
+      message: e.target.message.value
+    };
+    const JSONdata = JSON.stringify(data);
+    const endpoint = "/api/send";
+
+    console.log(data);
+    setEmailSubmitted(true);
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSONdata,
+    };
+    const response = await fetch(endpoint, options);
+
+    const resData = await response.json();
+
+    if(response.status === 200){
+      console.log("Email sent successfully");
+    } else {
+      console.log("Email failed to send");
+   }
+  };
+
   // Function to handle opening links in a new tab
   const openInNewTab = (url) => {
     window.open(url, '_blank', 'noopener,noreferrer');
   };
   return (
-    <section  id="contact" className="grid md:grid-cols-2 my-12 md:my-16 py-24 gap-4 relative">
+    <section id="contact" className="grid md:grid-cols-2 my-12 md:my-16 py-24 gap-4 relative">
       <div className="absolute w-80 h-80 z-0 "></div>
       <div className="z-10">
         <h5 className="text-xl font-bold text-white myåç-2">Let's Connect</h5><br></br>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
-      
-I am always open to exploring new opportunities and partnerships. If you have a project in mind, a job opportunity, or just wish to discuss an idea, feel free to reach out.
-<br></br>
-I'm excited about the possibility of working together and discovering what we can achieve.
+
+          I am always open to exploring new opportunities and partnerships. If you have a project in mind, a job opportunity, or just wish to discuss an idea, feel free to reach out.
+          <br></br>
+          I'm excited about the possibility of working together and discovering what we can achieve.
         </p>
         <div className="flex gap-2">
           <div className="cursor-pointer" onClick={() => openInNewTab('https://github.com/ashuppal')}>
@@ -42,59 +76,68 @@ I'm excited about the possibility of working together and discovering what we ca
           </div>
         </div>
       </div>
-        <form>
-          <div class="mb-6">
-            <label
-              for="email"
-              class="block mb-2 text-sm font-medium text-white"
-            >
-              Your email
-            </label>
-            <input
-              type="email"
-              id="email"
-              class="bg-gray-[#18191E] border border-[#33353F] bg-[#18191E] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg  block w-full p-2.5"
-              
-              required
-            />
-          </div>
-          <div class="mb-6">
-            <label
-              for="subject"
-              class="block mb-2 text-sm font-medium text-white"
-            >
-              Subject
-            </label>
-            <input
-              type="text"
-              id="subject"
-              class="bg-gray-[#18191E] border border-[#33353F] bg-[#18191E] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg  block w-full p-2.5"
-              
-            />
-          </div>
-          <div class="mb-6">
-            <label
-              for="subject"
-              class="block mb-2 text-sm font-medium text-white"
-            >
-              Message
-            </label>
-            <textarea
-              id="message"
-              class="bg-gray-[#18191E] border border-[#33353F] bg-[#18191E] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg  block w-full p-2.5"
-            
-            />
-          </div>
-          <div class="mb-6">
-            <button
-              type="submit"
-              class="bg-gradient-to-r from-cyan-500 to-lime-500 text-white font-medium py-2.5 px-5 rounded-lg w-full"
-            >
-              {" "}
-              Send message{" "}
-            </button>
-          </div>
-        </form>
+      {emailSubmitted ? (
+          <p className="text-green-500 text-sm mt-2">
+            Email sent successfully!
+          </p>
+        ) : (
+      <form className='flex flex-col' onSubmit={handleSubmit}>
+        <div class="mb-6">
+          <label
+            htmlfor="email"
+            class="block mb-2 text-sm font-medium text-white"
+          >
+            Your email
+          </label>
+          <input
+            name="email"
+            type="email"
+            id="email"
+            class="bg-gray-[#18191E] border border-[#33353F] bg-[#18191E] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg  block w-full p-2.5"
+
+            required
+          />
+        </div>
+        <div class="mb-6">
+          <label
+            htmlfor="subject"
+            class="block mb-2 text-sm font-medium text-white"
+          >
+            Subject
+          </label>
+          <input
+            name="subject"
+            type="text"
+            id="subject"
+            class="bg-gray-[#18191E] border border-[#33353F] bg-[#18191E] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg  block w-full p-2.5"
+
+          />
+        </div>
+        <div class="mb-6">
+          <label
+            htmlfor="subject"
+            class="block mb-2 text-sm font-medium text-white"
+          >
+            Message
+          </label>
+          <textarea
+            name="message"
+            id="message"
+            class="bg-gray-[#18191E] border border-[#33353F] bg-[#18191E] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg  block w-full p-2.5"
+
+          />
+        </div>
+        <div class="mb-6">
+          <button
+            type="submit"
+            class="bg-gradient-to-r from-cyan-500 to-lime-500 text-white font-medium py-2.5 px-5 rounded-lg w-full"
+          >
+            {" "}
+            Send message{" "}
+          </button>
+        </div>
+      </form>
+      )}
 
     </section>
   );
